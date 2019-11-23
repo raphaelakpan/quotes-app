d5db13487e66839b6790db915f898a49aebd1247
Raphael Akpan <raphael.akpan@andela.com>
2019-11-23 06:09:56 +0100

    Add font, images, create color variables

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,13 +45,11 @@ Quote.create(
 )
 
 Quote.create(
-  content: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma - which is living with the results of other people's thinking. Don't let the noise of others' opinions drown out your own inner voice. And most important, have the courage to follow your heart and intuition.
-  Read more at https://www.brainyquote.com/authors/steve-jobs-quotes",
+  content: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma - which is living with the results of other people's thinking. Don't let the noise of others' opinions drown out your own inner voice. And most important, have the courage to follow your heart and intuition",
   author: authors.last
 )
 Quote.create(
-  content: "My favorite things in life don't cost any money. It's really clear that the most precious resource we all have is time.
-  Read more at https://www.brainyquote.com/authors/steve-jobs-quotes",
+  content: "My favorite things in life don't cost any money. It's really clear that the most precious resource we all have is time",
   author: authors.last
 )
 
